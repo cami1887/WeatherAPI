ddeceb7bcd4f930364568e131749898ac6fcc777
Camille Lacaden <cami1887@gmail.com>
2024-09-11 23:30:18 -0700

    added more specified api calls

--- index.js
+++ index.js
@@ -1,11 +1,74 @@
+
+import { usInfo } from "./US_States_and_Cities.js";
 const APIKey = '3792aefde03b401da81232716240808';
-let locationInput = 'united states portland oregon';
-const timeInput = document.querySelector('.time');
+const locationInput = document.getElementById('state');
+const cityInput = document.getElementById('city');
+const timeOutput = document.getElementById('time');
 const uvDescription = document.querySelector('.UV-description');
 const clothingDescription = document.querySelector('.clothing-description');
 const mentalHealthDescription = document.querySelector('.mental-health-description');
 const dogWalkingDescription = document.querySelector('.dog-walking-description');
 const cosmicEventDescription = document.querySelector('.cosmic-events-description');
+const statesArray = Object.keys(usInfo).sort();
+let locationParameter = '';
+
+fetch('http://api.weatherapi.com/v1/current.json?key='+APIKey+'&q='+'Alabama Auburn').then((response) => response.json()).then((data)=> console.log(data));
+
+statesArray.forEach((state) => {
+    const option = document.createElement("option");
+    option.textContent = state;
+    option.setAttribute("value", state);
+    locationInput.appendChild(option);
+})
+
+locationInput.addEventListener('input', displayCities);
+cityInput.addEventListener('input', displayWeather);
+
+function displayCities(e) {
+    while (cityInput.firstChild) {
+        cityInput.removeChild(cityInput.firstChild);
+    }
+    let state = e.target.value;
+    if (state != 'select') {
+        let city = Object.values(Object.values(usInfo));
+        let states = Object.keys(usInfo);
+        let index = states.indexOf(state)
+        let allCities = city[index]?.sort();
+        let weatherInfo = [];
+        allCities?.forEach((city) => {
+            const option = document.createElement("option");
+            option.textContent = city;
+            option.setAttribute("value", city);
+            cityInput.appendChild(option);
+        }
+    )
+    fetch('http://api.weatherapi.com/v1/current.json?key='+APIKey+'&q='+allCities[0])
+        .then((response) => 
+            response.json())
+        .then((data)=> 
+            displayText(data.current.temp_f, data.current.temp_c)
+            // weatherInfo = [data.current.temp_f, data.current.temp_c]
+            // console.log(Object.entries(data.current.temp_c))
+        )
+        // testFuncton(weatherInfo)
+        // console.log(weatherInfo)
+    }
+
+    else {
+        return false;
+    }
+}
+
+function displayText(f, c) {
+    console.log(f,c)
+
+}
+
+function displayWeather(e) {
+    locationParameter = e.target.value;
+    fetch('http://api.weatherapi.com/v1/current.json?key='+APIKey+'&q='+locationParameter).then((response) => response.json()).then((data)=> console.log(data));
+}
+
 
 const allDescriptions = {
     uvIndex: [
@@ -25,8 +88,6 @@ const allDescriptions = {
         "There's a meteor shower, but it may be too cloudy to see :("
     ]
 }
-
-fetch('http://api.weatherapi.com/v1/current.json?key='+APIKey+'&q='+locationInput).then((response) => response.json()).then((data)=> console.log(data));
 
 
 uvDescription.innerText = allDescriptions.uvIndex[1];
